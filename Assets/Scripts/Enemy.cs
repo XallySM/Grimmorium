934bb05482008aced82adda1f0a190cc8070b780
AndresPaniagua-OjoDeJade-27 <a01733962@tec.mx>
2022-10-11 21:53:01 -0500

    AI additions 1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,10 +9,19 @@ public class Enemy : MonoBehaviour
     [SerializeField] Transform agentTarget;
 
     [SerializeField] float PathCalculateRefreshRate = .25f;
+    [SerializeField] float AttackDistance = 4.5f;
+    [SerializeField] float dashSpeed;
+    [SerializeField] float dashTime;
 
+    CharacterController EnemyCont;
+
+
+
+    float distanceToTarget;
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        EnemyCont = GetComponent<CharacterController>();
     }
 
     private void Start()
@@ -21,7 +30,7 @@ public class Enemy : MonoBehaviour
     }
     void Update()
     {
-        
+        CalculateDistanceToTarget();
     }
 
     IEnumerator UpdatePath()
@@ -35,4 +44,37 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    void CalculateDistanceToTarget()
+    {
+        distanceToTarget = Vector3.Distance(transform.position, agentTarget.position);
+        //Debug.Log(distanceToTarget);
+
+        /*if ( distanceToTarget <= AttackDistance)
+        {
+            StartCoroutine(DashAttack());
+        }*/
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ( other.CompareTag("Player"))
+        {
+            StartCoroutine(DashAttack());
+        }
+    }
+
+    IEnumerator DashAttack()
+    {
+        float startTime = Time.time;
+
+        while(Time.time < startTime + dashTime)
+        {
+            EnemyCont.Move(transform.forward * dashSpeed * Time.deltaTime);
+
+            
+
+            yield return null;
+        }
+
+    }
 }
