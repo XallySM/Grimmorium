44fb1b5cee760d4ac5293efe1326651f2e428cb9
XallySM <115589729+XallySM@users.noreply.github.com>
2022-10-21 18:42:03 -0500

    Cambios

--- Assets/CarpetadeXally/Scripts/Juego/Items/Weapon/WeaponSlotManager.cs
+++ Assets/CarpetadeXally/Scripts/Juego/Items/Weapon/WeaponSlotManager.cs
@@ -3,45 +3,92 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-    public class WeaponSlotManager : MonoBehaviour
+public class WeaponSlotManager : MonoBehaviour
+{
+    public WeaponHolderSlot leftHandSlot;
+    //public WeaponHolderSlot rightHandSlot;
+
+    DamageCollider leftHandDamageCollider;
+    //DamageCollider rightHandDamageCollider;
+
+    private void Awake()
     {
-        public WeaponHolderSlot leftHandSlot;
-        //public WeaponHolderSlot rightHandSlot;
-
-        private void Awake()
+        WeaponHolderSlot[] weaponHolderSlots = GetComponentsInChildren<WeaponHolderSlot>();
+        foreach (WeaponHolderSlot weaponSlot in weaponHolderSlots)
         {
-            WeaponHolderSlot[] weaponHolderSlots = GetComponentsInChildren<WeaponHolderSlot>();
-            foreach (WeaponHolderSlot weaponSlot in weaponHolderSlots)
+            if (weaponSlot.isLeftHandSlot)
             {
-                if (weaponSlot.isLeftHandSlot)
-                {
-                    leftHandSlot = weaponSlot;
-                    Debug.Log("Esta en la izquierda");
-                }
-                /*else if (weaponSlot.isRightHandSlot)
-                {
-                    rightHandSlot = weaponSlot;
-                    Debug.Log("Esta en la derecha");
-                }
-                */
+                leftHandSlot = weaponSlot;
+                Debug.Log("Esta en la izquierda");
             }
-
-        }
-
-        public void LoadWeponOnSlot(WeaponItem weaponItem, bool isLeft)
-        {
-            if(isLeft)
+            /*else if (weaponSlot.isRightHandSlot)
             {
-                leftHandSlot.LoadWeaponModel(weaponItem);
-                Debug.Log("Esta en la izquierda el modelo");
-
-            }
-            /*else 
-            {
-                rightHandSlot.LoadWeaponModel(weaponItem);
-                Debug.Log("Esta en la derecha el modelo");
+                rightHandSlot = weaponSlot;
+                Debug.Log("Esta en la derecha");
             }
             */
         }
+
     }
+
+    public void LoadWeponOnSlot(WeaponItem weaponItem, bool isLeft)
+    {
+        if (isLeft)
+        {
+            leftHandSlot.LoadWeaponModel(weaponItem);
+            LoadLeftWeaponDamageCollider();
+
+        }
+        /*else 
+        {
+            rightHandSlot.LoadWeaponModel(weaponItem);
+            Debug.Log("Esta en la derecha el modelo");
+            LoadLeftWeaponDamageCollider();
+        }
+        */
+    }
+
+    #region Handle Weapon´s Damage Colliders
+
+    private void LoadLeftWeaponDamageCollider()
+    {
+
+        leftHandDamageCollider = leftHandSlot.currentWeaponModel.GetComponentInChildren<DamageCollider>();
+
+    }
+
+    /*private void LoadRightWeaponDamageCollider()
+    {
+
+        rightHandDamageCollider = rightHandSlot.currentWeaponModel.GetComponentInChildren<DamageCollider>();
+
+    }
+    */
+
+    public void OpenLeftHandDamageCollider()
+    {
+        leftHandDamageCollider.EnableDamageCollider();
+    }
+
+    /*public void OpenRightHandDamageCollider()
+    {
+        rightHandDamageCollider.EnableDamageCollider();
+    }
+    */
+
+    public void CloseLeftHandDamageCollider()
+    {
+        leftHandDamageCollider.DisableDamageCollider();
+    }
+
+    /*public void CloseRightHandDamageCollider()
+    {
+        rightHandDamageCollider.DisableDamageCollider();
+    }
+    */
+
+    #endregion
+
+}
+
 
